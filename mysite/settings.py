211d57465b580ac9bf033362076fe94a8b032544
NandhuT17 <nandhakishor261@gmail.com>
2026-01-27 19:53:20 +0530

    Added user authentication functionality

--- mysite/settings.py
+++ mysite/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'base.apps.BaseConfig',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -115,3 +116,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+
+
+LOGIN_URL = 'login'
+LOGIN_REDIRECT_URL = "home"
+LOGOUT_REDIRECT_URL = "logout"
---
+++ users/views.py
@@ -0,0 +1,49 @@
+from django.shortcuts import render,redirect
+from django.contrib import messages
+from django.contrib.auth.models import User
+from django.contrib.auth import login,logout,authenticate
+from django.contrib.auth.decorators import login_required
+# Create your views here.
+
+
+
+def register_user(request) :
+    if request.method == "POST" :
+        username = request.POST['username']
+        email = request.POST['email']
+        password1 = request.POST['password1']
+        password2 = request.POST['password2']
+
+        if password1!=password2:
+            messages.error(request,"The passwords doesn't match")
+            return redirect('register')
+        
+        user = User.objects.create_user(
+            username,
+            email,
+            password1,
+        )
+        login(request,user)
+        messages.success(request,"You are logged in successfully")
+        return redirect('home')
+    return render(request,'users/register.html')
+
+
+def login_user(request) :
+    if request.method == "POST" :
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(request,username=username,password=password)
+
+        if user is not None :
+            login(request,user)
+            return redirect('home')
+        else :
+            messages.error(request,"Invalid Credentials")
+            return redirect('login')
+    return render(request,'users/login.html')
+
+def logout_user(request) :
+    logout(request)
+    return redirect('home')
