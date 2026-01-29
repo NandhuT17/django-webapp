1232407704214de8042d660b9698dd26d76c2572
NandhuT17 <nandhakishor261@gmail.com>
2026-01-29 23:13:23 +0530

    style(CSS) : Implemented CSS styles to authentication page

--- users/views.py
+++ users/views.py
@@ -31,10 +31,10 @@ def register_user(request) :
 
 def login_user(request) :
     if request.method == "POST" :
-        username = request.POST['username']
+        email = request.POST['email']
         password = request.POST['password']
 
-        user = authenticate(request,username=username,password=password)
+        user = authenticate(request,email=email,password=password)
 
         if user is not None :
             login(request,user)
