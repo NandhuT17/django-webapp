daf567754997913b2a16ffc61e1a8a3dc1a33400
NandhuT17 <nandhakishor261@gmail.com>
2026-02-19 22:30:20 +0530

    corrections in models

--- products/models.py
+++ products/models.py
@@ -1,24 +1,33 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Product(models.Model):
 
-class Product(models.Model) :
-
-    class CategoryChoice(models.TextChoices) :
-        beverages = "Beverages","Beverages"
-        electonics = "Electronics","Electronics"
-        fashion = "Fashion","Fashion"
-        grocery = "Grocery","Grocery"
-        personal_care = "Personal Care","Personal Care"
-        snacks = "Snacks","Snacks"
-        stationary = "Stationary","Stationary"
-        others = "Others","Others"
+    class CategoryChoice(models.TextChoices):
+        BEVERAGES = "Beverages", "Beverages"
+        ELECTRONICS = "Electronics", "Electronics"
+        FASHION = "Fashion", "Fashion"
+        GROCERY = "Grocery", "Grocery"
+        PERSONAL_CARE = "Personal Care", "Personal Care"
+        SNACKS = "Snacks", "Snacks"
+        STATIONARY = "Stationary", "Stationary"
+        OTHERS = "Others", "Others"
 
     product_name = models.CharField(max_length=50)
     product_price = models.IntegerField()
-    product_category = models.CharField(choices=CategoryChoice.choices,default="Others")
+    product_category = models.CharField(
+        max_length=30,
+        choices=CategoryChoice.choices,
+        default=CategoryChoice.OTHERS
+    )
     product_stock = models.IntegerField(default=0)
     product_desc = models.TextField(max_length=800)
-    product_image = models.CharField(max_length=255,default="https://support.ptc.com/help/thingworx/platform/r9/en/ThingWorx/images/ImageWidgetBeta.png")
+    product_image = models.CharField(
+        max_length=255,
+        default="https://support.ptc.com/help/thingworx/platform/r9/en/ThingWorx/images/ImageWidgetBeta.png"
+    )
+
+
+
 
     
