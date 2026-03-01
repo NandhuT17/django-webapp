fb84601fd19b1ea82a4006c4f3a0a06967e1a302
NandhuT17 <nandhakishor261@gmail.com>
2026-03-01 19:38:13 +0530

    added allowed host

--- mysite/settings.py
+++ mysite/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-du8w^n(q)%hk(7h6pkdu^!uds+)si&l84j=o_l&wk_vn6+iwyu
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["https://pymart.onrender.com/"]
 
 
 # Application definition
