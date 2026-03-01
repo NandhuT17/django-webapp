28c3b2ae4c24c9edd3202d75b5d0a0c0d2f8539c
NandhuT17 <nandhakishor261@gmail.com>
2026-03-01 17:44:52 +0530

    corrections

--- build.sh
+++ build.sh
@@ -4,9 +4,8 @@ pip install -r requirements.txt
 
 python manage.py migrate
 
-python manage.py createsuperuser \
---noinput \
---username $DJANGO_SUPERUSER_USERNAME \
---email $DJANGO_SUPERUSER_EMAIL || true
+echo "from django.contrib.auth import get_user_model;
+User=get_user_model();
+User.objects.filter(username='admin').exists() or User.objects.create_superuser('admin','admin@gmail.com','admin123')" | python manage.py shell
 
 python manage.py collectstatic --noinput
