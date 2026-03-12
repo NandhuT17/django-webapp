9ddf20366b24178d1d33e2c908291e75ebcc1558
NandhuT17 <nandhakishor261@gmail.com>
2026-03-12 19:39:37 +0530

    refactor(settings) : default storage

--- mysite/settings.py
+++ mysite/settings.py
@@ -176,4 +176,8 @@ CLOUDINARY_STORAGE = {
     "API_SECRET": os.getenv("CLOUDINARY_API_SECRET"),
 }
 
-DEFAULT_FILE_STORAGE = "cloudinary_storage.storage.MediaCloudinaryStorage"
+STORAGES = {
+    "default": {
+        "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
+    }
+}
