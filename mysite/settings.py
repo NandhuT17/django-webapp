bf417949301f078c96583b33e1b39c81ad3dc4b7
NandhuT17 <nandhakishor261@gmail.com>
2026-03-12 19:00:20 +0530

    feat(images) : add cloudinary media storage

--- mysite/settings.py
+++ mysite/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 import os
 from dotenv import load_dotenv
 import dj_database_url
+import cloudinary
 
 load_dotenv()
 
@@ -42,6 +43,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cloudinary',
+    'cloudinary_storage',
     'products',
     'users',
     'dashboard',
@@ -158,3 +161,13 @@ TEST_SECRET_KEY = os.getenv("TEST_SECRET_KEY")
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
+
+
+cloudinary.config(
+    cloud_name = os.getenv("CLOUDINARY_CLOUD_NAME"),
+    api_key = os.getenv("CLOUDINARY_API_KEY"),
+    api_secret = os.getenv("CLOUDINARY_API_SECRET"),
+    secure = True
+)
+
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
