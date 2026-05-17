f49e2187d340c28ee41060f9a08d8cd0da5d6d98
NandhuT17 <nandhakishor261@gmail.com>
2026-05-17 19:30:38 +0530

    replced smtp to resend

--- mysite/settings.py
+++ mysite/settings.py
@@ -149,12 +149,13 @@ LOGOUT_REDIRECT_URL = "logout"
 
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST = 'smtp.resend.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.getenv('EMAIL_USER')
-EMAIL_HOST_PASSWORD = os.getenv('EMAIL_SECRET_PASSWORD')
+EMAIL_HOST_USER = 'resend'
+EMAIL_HOST_PASSWORD = os.getenv('RESEND_API_KEY')
 EMAIL_TIMEOUT = 10
+DEFAULT_FROM_EMAIL = 'onboarding@resend.dev'
 
 
 TEST_API_KEY = os.getenv("TEST_API_KEY")
