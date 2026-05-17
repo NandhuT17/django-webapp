1709dde4f6c587562d15d571b6a33b1a38740059
NandhuT17 <nandhakishor261@gmail.com>
2026-05-17 19:12:06 +0530

    bug fixes

--- products/views.py
+++ products/views.py
@@ -211,22 +211,3 @@ def checkout(request) :
     return render(request,'products/checkout.html',context)
 
 
-
-
-
-User = get_user_model()
-
-username = os.getenv("DJANGO_SUPERUSER_USERNAME")
-email = os.getenv("DJANGO_SUPERUSER_EMAIL")
-password = os.getenv("DJANGO_SUPERUSER_PASSWORD")
-
-if username and email and password:
-    if not User.objects.filter(username=username).exists():
-        User.objects.create_superuser(
-            username=username,
-            email=email,
-            password=password
-        )
-        print("Superuser created.")
-    else:
-        print("Superuser already exists.")
