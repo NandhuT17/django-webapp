520ddd9baf6c9198aea2dca624a2c1052a3e3a36
NandhuT17 <nandhakishor261@gmail.com>
2026-05-22 19:15:02 +0530

    corrections

--- users/views.py
+++ users/views.py
@@ -3,8 +3,6 @@ from django.contrib import messages
 from django.contrib.auth.models import User
 from django.contrib.auth import login,logout,authenticate
 from django.contrib.auth.decorators import login_required
-from django.conf import settings
-from django.core.mail import send_mail
 # Create your views here.
 
 
